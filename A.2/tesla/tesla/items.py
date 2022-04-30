c7c635e8a8a907a4ec0c40f21f97b987c01e78b7
zaphodnothingth <zaphodnothingth@gmail.com>
2022-04-30 12:46:04 -0400

    deep competitor scrape

--- A.2/tesla/tesla/items.py
+++ A.2/tesla/tesla/items.py
@@ -3,16 +3,16 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
 
-class TeslaItem(scrapy.Item):
+class TeslaItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    url = scrapy.Field()
-    name = scrapy.Field()
-    title = scrapy.Field()
-    body = scrapy.Field()
-    content = scrapy.Field()
+    url = Field()
+    name = Field()
+    title = Field()
+    body = Field()
+    keywords = Field()
+    content = Field()
 
-    pass
--- A.2/tesla/tesla/spiders/spider_competitors.py
+++ A.2/tesla/tesla/spiders/spider_competitors.py
@@ -1,39 +1,31 @@
-from scrapy.spider import BaseSpider
-from scrapy.selector import HtmlXPathSelector
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+import json
 
 # local
 from tesla.items import TeslaItem
 
-class TeslaSpider(BaseSpider):
+class TeslaSpider(scrapy.Spider):
 
     name = "competitors"
     # allowed_domains = ["deloitte.com"]
 
-    with open("../companies.jsonl") as f:
+    with open("data/companies.jsonl") as f:
         start_urls = [json.loads(line)['url'] 
                         for line in f.readlines()]
-
+   
     def parse(self, response):
+        for link in LinkExtractor().extract_links(response):
+            url = link.url
+            yield scrapy.Request(url, callback = self.parse_dir_contents)
 
-        hxs = HtmlXPathSelector(response)
-        sites = hxs.select('//table[@class="custom_table"]/tr')
+    def parse_dir_contents(self, response):
+        
+        item = TeslaItem()
 
-        items = []
+        item['url'] = response.url
+        item['name'] = ''.join(response.selector.xpath('./td/a/text()').extract())
+        item['title'] = response.selector.xpath('//title/text()').extract()  # [0].split('|')[1].strip()
+        item['body'] = response.body
 
-        for site in sites:
-
-            #print site
-            item = TeslaItem()
-
-            item['name'] = ''.join(site.select('./td/a/text()').extract())
-            item['url'] = ''.join(site.select('./td/a/@href').extract())
-            # item['title'] = ''.join(site.select('./td[4]/text()').extract())
-            item['title'] = site.select('//title/text()').extract()  # [0].split('|')[1].strip()
-            
-            # use xpath differently
-            item['content'] = response.xpath('//div[@id=$value]/a/text()', value='content').get()
-
-
-            items.append(item)
-
-        return items
+        yield item
